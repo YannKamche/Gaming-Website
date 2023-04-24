2e490295cd4ad1c46e82b26a2e738c8881799309
Yann Kamche <yannkamche@gmail.com>
2023-04-24 23:05:34 +0100

    Modified

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
  * add event on multiple elements
  */
 const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0; len = elements.length; i < len, i++)  {
+    for (let i = 0; len = elements.length; i < len; i++)  {
         elements[i].addEventListener (eventType, callback);
     }
 }
